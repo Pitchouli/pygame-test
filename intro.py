bf940c9a4060339cc7e54ee874b9424f3d1b9203
Toby Cayce <70346578+toby-ctrlaltdelete@users.noreply.github.com>
2021-05-03 10:54:00 -0700

    Add files via upload
    time to add the ability to have buttons that can be clicked

--- intro.py
+++ intro.py
@@ -1,6 +1,24 @@
 # import the pygame module, so you can use it
 import pygame
- 
+
+# Code I found that adds button functionality
+def button(msg,x,y,w,h,ic,ac,action=None):
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+    print(click)
+    if x+w > mouse[0] > x and y+h > mouse[1] > y:
+        pygame.draw.rect(gameDisplay, ac,(x,y,w,h))
+
+        if click[0] == 1 and action != None:
+            action()         
+    else:
+        pygame.draw.rect(gameDisplay, ic,(x,y,w,h))
+
+    smallText = pygame.font.SysFont("comicsansms",20)
+    textSurf, textRect = text_objects(msg, smallText)
+    textRect.center = ( (x+(w/2)), (y+(h/2)) )
+    gameDisplay.blit(textSurf, textRect)
+
 # define a main function
 def main():
      
@@ -19,8 +37,12 @@ def main():
     screen.fill((255, 255, 255))
 
     # Draw a solid blue circle in the center
-    pygame.draw.circle(screen, (0, 0, 255), (25, 25), 75)
-
+	# Parameters: surface, color, center point, radius
+    pygame.draw.circle(screen, (0, 0, 255), (90, 120), 20)
+	
+	# Add the button at the bottom
+            button("test",150,450,100,50,green,bright_green,buttontest)
+	
     # Update the window, because I guess that's not built-in
     pygame.display.flip()
 	
